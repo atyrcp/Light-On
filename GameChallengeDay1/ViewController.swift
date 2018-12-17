e645c65b781ce2b9d16d8fbc1ca0ad2a73a2bf98
z <z@zde-macbook-pro.local>
2018-12-17 09:45:16 +0800

    Initial Commit

---
+++ GameChallengeDay1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameChallengeDay1
+//
+//  Created by z on 2018/12/17.
+//  Copyright © 2018年 z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
